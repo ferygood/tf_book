efdcc9a7bff39a0b9ee3a5c3528ed91a99605d31
yaochung41 <yaochung41@gmail.com>
2022-04-08 01:35:16 +0200

    verbose=0

--- _build/jupyter_execute/contents/one-neuron.py
+++ _build/jupyter_execute/contents/one-neuron.py
@@ -23,6 +23,9 @@ model.compile(optimizer='sgd', loss='mean_squared_error')
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
-model.fit(xs, ys, epochs=500)
-model.predict([10.0])
+model.fit(xs, ys, epochs=500, verbose=0)
+print(model.predict([10.0]))
 
+
+# 參考資料：  
+# [TensorFlow YouTube 影片](https://www.youtube.com/watch?v=OXNC_sefxi4&list=PLQY2H8rRoyvwr-3IlvJXA1JyOlpcbIGa1&index=1&ab_channel=TensorFlow)
