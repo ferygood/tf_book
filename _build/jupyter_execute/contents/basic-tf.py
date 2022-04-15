5becf3852f5135f49ad596ac55bd6cf4a2ee6fab
yaochung41 <yaochung41@gmail.com>
2022-04-15 23:38:00 +0200

    structure table content

---
+++ _build/jupyter_execute/contents/basic-tf.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Basic TensorFlow examples  
+# Here we show the basic tensorflow examples:  
+# 1. [單顆神經元範例 One-neuron example](one-neuron.ipynb)
+# 2. [Fashion MNIST 基礎分類](fmnist.ipynb)  
+# 3. [IMDB 評論分類](imdb.ipynb)
